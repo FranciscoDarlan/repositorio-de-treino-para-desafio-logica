a05b3d1f3624ced304a1f2789795d7f71c4c1375
FranciscoDarlan <darlann_c_jpa@hotmail.com>
2024-05-07 20:45:46 -0300

    logica confirindo numeros positivos e negativos

---
+++ script.js
@@ -0,0 +1,23 @@
+let inputText = document.querySelector('.input-text');
+let inputButton = document.querySelector('.input-button');
+let span = document.querySelector('.class-span');
+
+function validandoValor() {
+    let ValorDigitadoNoInput = inputText.value;
+
+    if (ValorDigitadoNoInput > 0) {
+        span.innerHTML = `O Número ${ValorDigitadoNoInput} ele é positivo!`
+    }else if (ValorDigitadoNoInput < 0) {
+        span.innerHTML = `O Número ${ValorDigitadoNoInput} ele é Negativo!`
+   
+    }else if (ValorDigitadoNoInput == 0) {
+            span.innerHTML = `O Número ${ValorDigitadoNoInput} não é nem negativo nem positivo!`
+       
+   
+
+    } else {
+        span.innerHTML = `O valor ( ${ValorDigitadoNoInput} ) não é Número! Por Favor digite um número`
+    }
+}
+
+inputButton.addEventListener('click', validandoValor);
